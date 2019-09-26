fc70f793f520d277374b75d92bb0e22a9b77b595
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-26 14:56:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1=1; 
+let num2=62;
+let multiply = (num1 * num2).toString();
+
+let random = Math.floor(Math.random() *10) +1;
+
+let mod = (4 % 8).toString();
+let max =Math.max(1,10,15,20).toString();
